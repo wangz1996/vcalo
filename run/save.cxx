7bfb11cdf89e30f86525969659e8bd1f01ea90e7
Zhen Wang <zhen.wang@cern.ch>
2024-09-12 15:47:24 +0800

    remove run folder

--- run/save.cxx
+++
@@ -1,5 +0,0 @@
-void save(TString name = "vcalo.json"){
-	auto f = TFile::Open("test.root","READ");
-	auto geo = (TGeoManager*)f->Get("vcalo");
-	geo->SaveAs(name);
-}
--- run/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/bin/time -v -p vcalo -c default.yaml #> log 2>&1
-#root -l -b -q save.cxx
-#scp vcalo.json zhenw@lxplus.cern.ch:/eos/user/z/zhenw/website/time_range
