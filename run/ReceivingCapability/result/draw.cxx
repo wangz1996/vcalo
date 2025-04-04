7c6718da79d5220e9478a15659c08739524540f2
Zhen Wang <wangz1996@sjtu.edu.cn>
2025-04-04 09:42:33 +0800

    script saving

--- run/ReceivingCapability/result/draw.cxx
+++ run/ReceivingCapability/result/draw.cxx
@@ -1,5 +1,7 @@
-void cutflow(){
-	ROOT::RDataFrame df("vtree","/home/wangzhen/mnt2/VLASTP/vcalo/run/OOC/result/ooc_plane_z.root");
+void draw(){
+	ROOT::RDataFrame df("vtree","test.root");
+	//TH1D *h_trigger = new TH1D("h_trigger","Trigger",1,0,1);
+	//TH1D *h_total = new TH1D("h_total","Total",1,0,1);
 	auto dff = df
 	.Filter([](const std::vector<float>& acd_e,const std::vector<float>& conv_e,const std::vector<float>& tracker_hite,const std::vector<float>& ecal_celle){
 		bool pass=false;
@@ -12,4 +14,9 @@ void cutflow(){
 	},{"acd_e","conv_e","tracker_hite","ecal_celle"},"Trigger")
 	.Filter("ecal_celle.size()==1","passECAL");
 	dff.Report()->Print();
+	//df.Foreach([&h_total](){h_total->Fill(0);});
+	//dff.Foreach([&h_trigger](){h_trigger->Fill(0);});
+	//TEfficiency *he = new TEfficiency(*h_trigger,*h_total);
+	//std::cout<<*dff.Count()<<" / "<<*df.Count()<<" = "<<he->GetEfficiency(1)<<" "<<he->GetEfficiencyErrorUp(1)<<" "<<he->GetEfficiencyErrorLow(1)<<std::endl;
+	//.Filter("");
 }
